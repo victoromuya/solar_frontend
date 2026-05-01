6abf955ad6895d5643796745bb1ad6abdcc293e4
unknown <viczik16@gmail.com>
2026-05-01 13:16:05 +0100

    added com

--- src/api/solarApi.js
+++ src/api/solarApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "https://solar-api-seven.vercel.app";
 
 const API = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`,
